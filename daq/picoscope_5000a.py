dc4d037c0d7d84a93e997aaf6992e6b7d9cd8df9
David Fokkema <davidfokkema@gmail.com>
2018-11-10 16:43:17 +0100

    Linter fixes

--- daq/picoscope_5000a.py
+++ daq/picoscope_5000a.py
@@ -1,3 +1,11 @@
+"""Interface to a 5000 Series PicoScope.
+
+Classes
+-------
+PicoScope5000A
+    Interface to a 5000 Series PicoScope.
+"""
+
 import ctypes
 
 from picosdk.ps5000a import ps5000a as ps
@@ -23,6 +31,8 @@ class PicoScope5000A:
         close the device
     """
 
+    _handle = None
+
     def __init__(self, serial=None, resolution_bits=12):
         """Instantiate the class and open the device."""
 
@@ -36,7 +46,7 @@ class PicoScope5000A:
         """
 
         handle = ctypes.c_int16()
-        resolution = self._get_resolution_from_bits(resolution_bits)
+        resolution = _get_resolution_from_bits(resolution_bits)
         assert_pico_ok(ps.ps5000aOpenUnit(ctypes.byref(handle), serial,
                                           resolution))
 
@@ -48,12 +58,13 @@ class PicoScope5000A:
         assert_pico_ok(ps.ps5000aCloseUnit(self._handle))
         self._handle = None
 
-    def _get_resolution_from_bits(self, resolution_bits):
-        """Return the resolution from the number of bits."""
 
-        if resolution_bits in [8, 12, 14, 15, 16]:
-            res_name = f"PS5000A_DR_{resolution_bits}BIT"
-        else:
-            raise InvalidParameterError(f"Resolution {resolution_bits}-bits "
-                                        "not supported")
-        return ps.PS5000A_DEVICE_RESOLUTION[res_name]
+def _get_resolution_from_bits(resolution_bits):
+    """Return the resolution from the number of bits."""
+
+    if resolution_bits in [8, 12, 14, 15, 16]:
+        res_name = f"PS5000A_DR_{resolution_bits}BIT"
+    else:
+        raise InvalidParameterError(f"A resolution of {resolution_bits}-bits "
+                                    "is not supported")
+    return ps.PS5000A_DEVICE_RESOLUTION[res_name]
