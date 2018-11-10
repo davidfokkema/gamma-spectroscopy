0113db81a8d9dcfebedf123a677efaccbf15d303
David Fokkema <davidfokkema@gmail.com>
2018-11-10 16:50:58 +0100

    PyDocStyle fixes

--- daq/picoscope_5000a.py
+++ daq/picoscope_5000a.py
@@ -14,6 +14,7 @@ from picosdk.functions import assert_pico_ok
 
 class InvalidParameterError(Exception):
     """Error because of an invalid parameter."""
+
     pass
 
 
@@ -29,13 +30,13 @@ class PicoScope5000A:
         open the device
     close()
         close the device
+
     """
 
     _handle = None
 
     def __init__(self, serial=None, resolution_bits=12):
         """Instantiate the class and open the device."""
-
         self.open(serial, resolution_bits)
 
     def open(self, serial=None, resolution_bits=12):
@@ -44,7 +45,6 @@ class PicoScope5000A:
         :param serial: (optional) Serial number of the device
         :param resolution_bits: vertical resolution in number of bits
         """
-
         handle = ctypes.c_int16()
         resolution = _get_resolution_from_bits(resolution_bits)
         assert_pico_ok(ps.ps5000aOpenUnit(ctypes.byref(handle), serial,
@@ -54,14 +54,12 @@ class PicoScope5000A:
 
     def close(self):
         """Close the device."""
-
         assert_pico_ok(ps.ps5000aCloseUnit(self._handle))
         self._handle = None
 
 
 def _get_resolution_from_bits(resolution_bits):
     """Return the resolution from the number of bits."""
-
     if resolution_bits in [8, 12, 14, 15, 16]:
         res_name = f"PS5000A_DR_{resolution_bits}BIT"
     else:
