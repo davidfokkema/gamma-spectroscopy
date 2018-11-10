a68ae064c5b2d08009cb7edca00dd682bf69bcb0
David Fokkema <davidfokkema@gmail.com>
2018-11-10 16:31:29 +0100

    Docstrings

--- daq/picoscope_5000a.py
+++ daq/picoscope_5000a.py
@@ -5,12 +5,36 @@ from picosdk.functions import assert_pico_ok
 
 
 class InvalidParameterError(Exception):
+    """Error because of an invalid parameter."""
     pass
 
 
 class PicoScope5000A:
+    """Interface to a 5000 Series PicoScope.
+
+    This class encapsulates the low-level PicoSDK and offers a python-friendly
+    interface to a 5000 Series PicoScope (e.g. a PicoScope 5242D).
+
+    Methods
+    -------
+    open()
+        open the device
+    close()
+        close the device
+    """
 
     def __init__(self, serial=None, resolution_bits=12):
+        """Instantiate the class and open the device."""
+
+        self.open(serial, resolution_bits)
+
+    def open(self, serial=None, resolution_bits=12):
+        """Open the device.
+
+        :param serial: (optional) Serial number of the device
+        :param resolution_bits: vertical resolution in number of bits
+        """
+
         handle = ctypes.c_int16()
         resolution = self._get_resolution_from_bits(resolution_bits)
         assert_pico_ok(ps.ps5000aOpenUnit(ctypes.byref(handle), serial,
@@ -19,13 +43,17 @@ class PicoScope5000A:
         self._handle = handle
 
     def close(self):
+        """Close the device."""
+
         assert_pico_ok(ps.ps5000aCloseUnit(self._handle))
         self._handle = None
 
     def _get_resolution_from_bits(self, resolution_bits):
+        """Return the resolution from the number of bits."""
+
         if resolution_bits in [8, 12, 14, 15, 16]:
             res_name = f"PS5000A_DR_{resolution_bits}BIT"
         else:
             raise InvalidParameterError(f"Resolution {resolution_bits}-bits "
-                                         "not supported")
+                                        "not supported")
         return ps.PS5000A_DEVICE_RESOLUTION[res_name]
