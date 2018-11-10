9f9f188dd2a9c5e2eaa35bd340c5469fc107feef
David Fokkema <davidfokkema@gmail.com>
2018-11-10 20:24:04 +0100

    Implemented set_channel

--- daq/picoscope_5000a.py
+++ daq/picoscope_5000a.py
@@ -12,6 +12,22 @@ from picosdk.ps5000a import ps5000a as ps
 from picosdk.functions import assert_pico_ok
 
 
+INPUT_RANGES = {
+    0.01: '10MV',
+    0.02: '20MV',
+    0.05: '50MV',
+    0.1: '100MV',
+    0.2: '200MV',
+    0.5: '500MV',
+    1: '1V',
+    2: '2V',
+    5: '5V',
+    10: '10V',
+    20: '20V',
+    50: '50V',
+}
+
+
 class InvalidParameterError(Exception):
     """Error because of an invalid parameter."""
 
@@ -30,6 +46,8 @@ class PicoScope5000A:
         open the device
     close()
         close the device
+    set_channel()
+        Set up input channels
 
     """
 
@@ -57,12 +75,62 @@ class PicoScope5000A:
         assert_pico_ok(ps.ps5000aCloseUnit(self._handle))
         self._handle = None
 
+    def set_channel(self, channel, coupling_type, range, offset=0,
+                    is_enabled=True):
+        """Set up input channels.
+
+        :param channel: channel name ('A', 'B', etc.)
+        :param coupling_type: 'AC' or 'DC' coupling
+        :param range: (float) input voltage range in volts
+        :param offset: analogue offset of the input signal
+        :param is_enabled: enable or disable the channel
+        :type is_enabled: boolean
+
+        The input voltage range can be 10, 20, 50 mV, 100, 200, 500 mV, 1, 2,
+        5 V or 10, 20, 50 V, but is given in volts. For example, a range of
+        20 mV is given as 0.02.
+        """
+        channel = _get_channel_from_name(channel)
+        coupling_type = _get_coupling_type_from_name(coupling_type)
+        range = _get_range_from_value(range)
+        assert_pico_ok(ps.ps5000aSetChannel(self._handle, channel, is_enabled,
+                                            coupling_type, range, offset))
+
 
 def _get_resolution_from_bits(resolution_bits):
     """Return the resolution from the number of bits."""
     if resolution_bits in [8, 12, 14, 15, 16]:
-        res_name = f"PS5000A_DR_{resolution_bits}BIT"
+        def_name = f"PS5000A_DR_{resolution_bits}BIT"
     else:
         raise InvalidParameterError(f"A resolution of {resolution_bits}-bits "
                                     "is not supported")
-    return ps.PS5000A_DEVICE_RESOLUTION[res_name]
+    return ps.PS5000A_DEVICE_RESOLUTION[def_name]
+
+
+def _get_channel_from_name(channel_name):
+    """Return the channel from the channel name."""
+    if channel_name in ['A', 'B', 'C', 'D']:
+        def_name = f"PS5000A_CHANNEL_{channel_name}"
+    else:
+        raise InvalidParameterError(f"Channel {channel_name} is not supported")
+    return ps.PS5000A_CHANNEL[def_name]
+
+
+def _get_coupling_type_from_name(coupling_type_name):
+    """Return the coupling type from the coupling type name."""
+    if coupling_type_name in ['AC', 'DC']:
+        def_name = f"PS5000A_{coupling_type_name}"
+    else:
+        raise InvalidParameterError(f"Coupling type {coupling_type_name} is "
+                                    "not supported")
+    return ps.PS5000A_COUPLING[def_name]
+
+
+def _get_range_from_value(range):
+    """Return the range from the range in volts."""
+    if range in INPUT_RANGES:
+        range_name = INPUT_RANGES[range]
+        def_name = f"PS5000A_{range_name}"
+    else:
+        raise InvalidParameterError(f"Range {range} V is not supported")
+    return ps.PS5000A_RANGE[def_name]
