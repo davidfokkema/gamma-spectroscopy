96d4bae775f899baaafe7023ab4ed5f0138b0ae6
David Fokkema <davidfokkema@gmail.com>
2018-11-14 12:28:45 +0100

    Quick-and-dirty gamma daq

--- test_daq.py
+++ test_daq.py
@@ -1,19 +1,46 @@
-from importlib import reload
-import matplotlib.pyplot as plt
+import time
+
+import tables
 
 from daq import picoscope_5000a
 
-if 'dev' in globals():
-    dev.close()
-    reload(picoscope_5000a)
+
+timebase = 35
+pre_trigger_samples = 10
+post_trigger_samples = 90
+
+
+class Gammas(tables.IsDescription):
+
+    t = tables.TimeCol()
+    trace = tables.FloatCol(
+        shape=(pre_trigger_samples + post_trigger_samples,))
+
+
+data = tables.open_file('data.h5', 'w')
+table = data.create_table('/', 'events', Gammas)
 
 dev = picoscope_5000a.PicoScope5000A()
-dev.set_channel('A', 'DC', 10, offset=0)
-dev.set_trigger('A', .1, 'RISING_OR_FALLING')
+dev.set_channel('A', 'DC', .1, offset=0)
+dev.set_trigger('A', 0, 'FALLING')
 
-t, data = dev.run_block(0, 1000, timebase=2000, num_captures=10)
+N = 0
+t0 = time.time()
+row = table.row
+try:
+    while True:
+        t, trace = dev.run_block(pre_trigger_samples, post_trigger_samples,
+                                 timebase)
+        row['t'] = time.time()
+        row['trace'] = trace
+        row.append()
+        N += 1
+except KeyboardInterrupt:
+    pass
+t1 = time.time()
 
-plt.figure()
-plt.plot(t * 1e3, data.T)
-plt.xlabel('Time [ms]')
-plt.show()
+data.create_array('/', 't', t)
+
+print(f"Event rate was {N / (t1 - t0):.1f} Hz.")
+
+data.flush()
