fa185c6c0208f9bfc4c210e8a5b760d04a4e7dbf
David Fokkema <davidfokkema@gmail.com>
2018-11-28 16:37:52 +0100

    Implemented run/stop button

--- daq/gamma_spectroscopy_gui.py
+++ daq/gamma_spectroscopy_gui.py
@@ -1,6 +1,7 @@
 import ctypes
 import os.path
 import sys
+import time
 
 from PyQt5 import uic, QtWidgets, QtCore
 import pyqtgraph as pg
@@ -17,6 +18,7 @@ def create_callback(signal):
 
 class UserInterface(QtWidgets.QMainWindow):
 
+    start_run_signal = QtCore.pyqtSignal()
     new_data_signal = QtCore.pyqtSignal()
     plot_data_signal = QtCore.pyqtSignal(dict)
 
@@ -30,16 +32,14 @@ class UserInterface(QtWidgets.QMainWindow):
 
         self.init_ui()
 
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.start_run)
-        self.timer.start(100)
-
     def init_ui(self):
         pg.setConfigOption('background', 'w')
         pg.setConfigOption('foreground', 'k')
 
         ui_path = 'gamma_spectroscopy_gui.ui'
         uic.loadUi(ui_path, self)
+
+        self.start_run_signal.connect(self.start_run)
 
         self.new_data_signal.connect(self.fetch_data)
         self.callback = create_callback(self.new_data_signal)
@@ -50,30 +50,42 @@ class UserInterface(QtWidgets.QMainWindow):
         self.range_box.currentIndexChanged.connect(self.set_range)
         self.range_box.setCurrentIndex(6)
 
+        self.run_stop_button.clicked.connect(self.toggle_run_stop)
+
         self.show()
 
     @QtCore.pyqtSlot()
-    def start_run(self):
-        if self._is_running:
-            return
-        else:
+    def toggle_run_stop(self):
+        if not self._is_running:
             self._is_running = True
-            self.scope.set_up_buffers(20000)
-            self.scope.start_run(10000, 10000, 20, callback=self.callback)
+            self.start_run_signal.emit()
+            self.run_stop_button.setText("Stop")
+        else:
+            self._is_running = False
+            self.scope.stop()
+            self.run_stop_button.setText("Run")
+
+    @QtCore.pyqtSlot()
+    def start_run(self):
+        self.scope.set_up_buffers(2000)
+        self.scope.start_run(1000, 1000, 2000, callback=self.callback)
 
     @QtCore.pyqtSlot(int)
     def set_range(self, range_idx):
-        print(f"RANGE CHANGED to {range_idx}")
+        self.scope.stop()
         ranges = list(INPUT_RANGES.keys())
         range = ranges[range_idx]
         self.scope.set_channel('A', 'DC', range)
         self._range = range
+        # if self._is_running:
+        #     self.start_run_signal.emit()
 
     @QtCore.pyqtSlot()
     def fetch_data(self):
         t, data = self.scope.get_data()
-        self._is_running = False
         self.plot_data_signal.emit({'x': t, 'y': data[0]})
+        if self._is_running:
+            self.start_run_signal.emit()
 
     @QtCore.pyqtSlot(dict)
     def plot_data(self, data):
