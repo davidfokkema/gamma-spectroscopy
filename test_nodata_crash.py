c6b9c8419f623752e41c7032eb3ad4550e667842
David Fokkema <davidfokkema@gmail.com>
2018-11-28 21:05:10 +0100

    Triggering nasty bug
    When closing, the callback can receive PICO_OK, but there is no data avalaible and get_data will crash.

--- test_nodata_crash.py
+++ test_nodata_crash.py
@@ -9,16 +9,17 @@ from picosdk.constants import PICO_STATUS_LOOKUP
 def my_callback(handle, status, parameters):
     print("CALLBACK")
     print(f"{PICO_STATUS_LOOKUP[status]}")
+    dev.get_adc_data()
 
 
 dev = PicoScope5000A()
 dev.set_channel('A', 'DC', 1)
-dev.set_trigger('A', 1, 'RISING')
-dev.set_up_buffers(200, 1)
-dev.start_run(100, 100, 20, callback=my_callback)
-time.sleep(2)
+# dev.set_trigger('A', 1, 'RISING')
+dev.set_up_buffers(200, 10000)
+dev.start_run(100, 100, 20, num_captures=10000, callback=my_callback)
+# time.sleep(.5)
 print("ST", end=None)
 dev.stop()
 time.sleep(.1)
 print("OP")
-time.sleep(2)
+time.sleep(1)
