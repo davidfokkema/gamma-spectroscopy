f8f5c79c29ae2f3a25f792916bd10306a3a11e4f
David Fokkema <davidfokkema@gmail.com>
2019-07-17 12:58:12 +0200

    Test with C parameters

--- test_thread_timers.py
+++ test_thread_timers.py
@@ -5,8 +5,8 @@ import time
 
 def callback_factory(event):
     """Return callback that will signal event when called."""
-    @ctypes.CFUNCTYPE(None)  # , ctypes.c_int16, ctypes.c_int, ctypes.c_void_p)
-    def data_is_ready_callback():  # handle, status, parameters):
+    @ctypes.CFUNCTYPE(None, ctypes.c_int16, ctypes.c_int, ctypes.c_void_p)
+    def data_is_ready_callback(handle, status, parameters):
         """Signal that data is ready when called by PicoSDK."""
         print("data is ready")
         event.set()
@@ -18,7 +18,8 @@ if __name__ == '__main__':
     callback = callback_factory(timer_event)
 
     print(timer_event.is_set())
-    timer = Timer(1.0, callback)
+    timer = Timer(1.0, callback, (ctypes.c_int16(), ctypes.c_int(),
+                                  ctypes.c_void_p()))
     timer.start()
     print("Started timer.")
 
